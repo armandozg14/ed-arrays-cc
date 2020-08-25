04f94198f8bfc4c3fef6081b4ee5299aebbe261c
Jesus Armando Zamora Gamez <armando.zamora14@hotmail.com>
2020-08-25 07:53:15 -0700

    Mi commit

---
+++ src/main.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main()
+{
+    int numbers[6]{100, 87982, 3456, 465, 885, 86};
+
+    std::cout << "Array memmory: " << &numbers << std::endl;
+    std::cout << "Array memmory: " << numbers << std::endl;
+
+    /*for(int i = 0; i < 6; i++)
+    {
+        std::cout << &numbers[i] << "\t";
+    }*/
+
+    for(int i = 0; i < 6; i++)
+    {
+        std::cout << numbers + i << "\t";
+    }
+
+    std::cout << std::endl;
+
+    std::cin.get();
+    
+    return 0;
+}
